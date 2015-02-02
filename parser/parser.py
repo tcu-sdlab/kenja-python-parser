2da07281e4e5e1ac75f448bdc61c7dd2769f0a0a
Kenji Fujiwara <kenji-f@is.naist.jp>
2015-02-02 15:43:04 +0900

    use with statement

--- parser/parser.py
+++ parser/parser.py
@@ -6,7 +6,6 @@ from treewriter import TreeWriter
 class GitTreeCreator:
     def __init__(self, output_path):
         self.output_path = output_path
-        self.output_file = []
 
     def parse_sourcecode(self):
         src = sys.stdin.read()
@@ -17,11 +16,10 @@ class GitTreeCreator:
             root = ast.parse(src)
         except:
             root = []
-        self.output_file = open(self.output_path, 'w')
 
-        writer = TreeWriter(self.output_file)
-        writer.write_tree(root)
-        self.output_file.close()
+        with open(self.output_path, 'w') as output_file:
+            writer = TreeWriter(output_file)
+            writer.write_tree(root)
 
 
 def main():
