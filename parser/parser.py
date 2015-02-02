b96147312c8a35db32889dc2cb4ff7ff7dcc9ccf
Kenji Fujiwara <kenji-f@is.naist.jp>
2015-02-02 15:40:58 +0900

    removed self.root

--- parser/parser.py
+++ parser/parser.py
@@ -7,7 +7,6 @@ class GitTreeCreator:
     def __init__(self, output_path):
         self.output_path = output_path
         self.output_file = []
-        self.root = []
 
     def parse_sourcecode(self):
         src = sys.stdin.read()
@@ -15,13 +14,13 @@ class GitTreeCreator:
 
     def write_ast_as_filetree(self, src):
         try:
-            self.root = ast.parse(src)
+            root = ast.parse(src)
         except:
-            pass
+            root = []
         self.output_file = open(self.output_path, 'w')
 
         writer = TreeWriter(self.output_file)
-        writer.write_tree(self.root)
+        writer.write_tree(root)
         self.output_file.close()
 
 
